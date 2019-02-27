b46866ca84a6cfa3eb3a663218de0e1c44cf4c15
relberger <relberger96@gmail.com>
2019-02-27 08:10:56 -0500

    ad model

---
+++ buysellapplication/Models/AdItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public DateTime AddedOn { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
